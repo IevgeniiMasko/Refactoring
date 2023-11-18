76d3117495e022272f9398bbd7be6e69e0affe92
Ievgenii Masko <ievgeniimasko@Ievgeniis-MacBook-Pro.local>
2023-11-18 18:05:53 +0100

    refactor: moved renderPlainText to different file

--- src/refactored/index.ts
+++ src/refactored/index.ts
@@ -1,21 +1,8 @@
 import { Invoice, PlaysConfig } from '../data';
-import { usd } from './utils';
-import { StatementData, createStatementData } from './createStatementData';
+
+import { createStatementData } from './createStatementData';
+import { renderPlainText } from './views/renderPlainText';
 
 export function statement(invoice: Invoice, plays: PlaysConfig) {
   return renderPlainText(createStatementData(invoice, plays), plays);
-}
-
-export function renderPlainText(data: StatementData, plays: PlaysConfig) {
-  let result = `Statement for ${data.customerId}\n`;
-
-  for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
-      perf.audience
-    } seats)\n`;
-  }
-
-  result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
-  return result;
 }
