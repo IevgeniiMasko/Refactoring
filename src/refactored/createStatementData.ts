85da3513e62a589d0a1d491c1e72748185d90789
Ievgenii Masko <ievgeniimasko@Ievgeniis-MacBook-Pro.local>
2023-11-18 18:33:57 +0100

    refactor: initial performace calculator is create with amount and volumeCredit

--- src/refactored/createStatementData.ts
+++ src/refactored/createStatementData.ts
@@ -3,6 +3,7 @@ import { APerformance, Invoice, Play, PlayType, PlaysConfig } from '../data';
 export type EnrichedPerformance = APerformance & {
   play: Play;
   amount: number;
+  volumeCredits: number;
 };
 
 export type StatementData = {
@@ -30,11 +31,8 @@ export function createStatementData(invoice: Invoice, plays: PlaysConfig) {
   }
 
   function volumeCreditFor(aPerformance: APerformance) {
-    let result = Math.max(aPerformance.audience - 30, 0);
-    if (playFor(aPerformance).type === PlayType.comedy) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
+    return new PerfomanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredit;
   }
 
   function totalAmount(data: StatementData) {
@@ -46,37 +44,58 @@ export function createStatementData(invoice: Invoice, plays: PlaysConfig) {
   }
 
   function enrichPerformance(aPerformance: APerformance): EnrichedPerformance {
+    const calculator = new PerfomanceCalculator(
+      aPerformance,
+      playFor(aPerformance),
+    );
     const result = {
       ...aPerformance,
-      play: playFor(aPerformance),
-      amount: amountFor(aPerformance) / 100,
+      play: calculator.play,
+      amount: calculator.amount / 100,
+      volumeCredits: calculator.volumeCredit,
     };
     return result;
   }
 
   function amountFor(aPerformance: APerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case PlayType.tragedy:
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case PlayType.comedy:
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
+    return new PerfomanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function playFor(aPerformance: APerformance) {
     return plays[aPerformance.playId];
+  }
+}
+
+class PerfomanceCalculator {
+  constructor(public performance: APerformance, public play: Play) {}
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case PlayType.tragedy:
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case PlayType.comedy:
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
+
+  get volumeCredit() {
+    let result = Math.max(this.performance.audience - 30, 0);
+    if (this.play.type === PlayType.comedy) {
+      result += Math.floor(this.performance.audience / 5);
+    }
+    return result;
   }
 }
